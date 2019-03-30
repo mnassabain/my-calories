266b50be66a8571362709f185a96474ae26e65e7
NASSABAIN MARCO <marco.nassabain@etu.unistra.fr>
2019-03-30 21:34:38 +0100

    added fontawesome

--- src/main.js
+++ src/main.js
@@ -1,6 +1,16 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faBreadSlice } from '@fortawesome/free-solid-svg-icons'
+import { faDrumstickBite } from '@fortawesome/free-solid-svg-icons'
+import { faFish } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+library.add(faBreadSlice)
+library.add(faDrumstickBite)
+library.add(faFish)
+Vue.component('font-awesome-icon', FontAwesomeIcon)
+
 Vue.config.productionTip = false
 
 new Vue({
