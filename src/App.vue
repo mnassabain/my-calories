f3501fafa4b5ced9b43a7b8e97230ce43d65b83f
NASSABAIN MARCO <marco.nassabain@etu.unistra.fr>
2019-03-30 18:16:56 +0100

    added the overall calorie counter

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,12 @@
 <template>
   <div id="app">
+    
+    <div class="overall-calories">
+      <h2>1800 kcal</h2>
+      <hr>
+      <h3>2500 kcal</h3>
+    </div>
+
   </div>
 </template>
 
@@ -19,8 +26,48 @@ export default {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+
+  margin: 0;
+  padding: 0;
+  
+  box-sizing: border-box;
+
+  display: grid;
+  grid-template: 
+    'overall-calories overall-calories'
+    'pie-chart macros'
+    'meals meals';
+}
+
+.overall-calories
+{
+  grid-area: overall-calories;
+  margin: 0;
+  width: 33%;
+  justify-self: end;
+  text-align: right;
+  margin-right: 20px;
+  margin-top: 10px;
+}
+
+h2
+{
+  margin: 5px 0;
+  padding: 0;
+}
+
+
+h3
+{
+  opacity: 0.8;
+  margin: 5px 0;
+  padding: 0;
+}
+
+hr
+{
+  margin: 0;
+  padding: 0;
 }
 </style>
