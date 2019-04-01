519834a7634bae002f976c1d69c93d92047911e5
NASSABAIN MARCO <marco.nassabain@etu.unistra.fr>
2019-04-01 19:15:58 +0200

    added basic createMeal interface

--- src/views/CreateMeal.vue
+++ src/views/CreateMeal.vue
@@ -4,12 +4,22 @@
 
         <form>
             <div class="form-entry">
-                <label for="name">Name</label>
-                <input type="text">
+                <!-- <label for="name">Name</label> -->
+                <input type="text" placeholder="Name">
             </div>
 
-            <number-input :value="0" center controls class="nb-input"></number-input>
+            <div class="macros">
+                <number-input :min="0" size="small" placeholder="Carbs" inline center controls class="nb-input"></number-input>
+                <number-input :min="0" size="small" placeholder="Protein" inline center controls class="nb-input"></number-input>
+                <number-input :min="0" size="small" placeholder="Fats" inline center controls class="nb-input"></number-input>
 
+                <br><br>
+
+                <number-input size="small" placeholder="calories" :value="250" inline center readonly></number-input>
+                
+            </div>
+
+            <button id="create-button">Create</button>
 
         </form>
     </div>
@@ -39,13 +49,18 @@ export default {
 form
 {
     margin-top: 20px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
 }
 
 .form-entry
 {
     display: flex;
     flex-direction: column;
+    align-items: center;
 
+    margin-top: 15px;
     margin-bottom: 15px;
 }
 
@@ -62,8 +77,8 @@ form
     -moz-osx-font-smoothing: grayscale;
     color: #2c3e50;
 
-    width: 90%;
-    background-color: #efefefef;
+    width: 100%;
+    /* background-color: #efefefef; */
     border: none;
     font-size: 1em;
     padding: 7px 10px;
@@ -76,9 +91,30 @@ form
     border-bottom :3px solid #71B280;
 }
 
-.nb-input
+form .macros
 {
-    width: 33%;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
 }
+
+form .macros .nb-input
+{
+    margin-top: 20px;
+}
+
+#create-button
+{
+    margin-top: 30px;
+
+    border: none;
+    background-image: linear-gradient(to top right, #134E5E, #71B280);
+    color: white;
+    
+    padding: 15px 40px;
+    font-weight: bold;
+    font-size: 1.2em;
+}
+
 
 </style>
