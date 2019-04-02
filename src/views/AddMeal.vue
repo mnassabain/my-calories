e794a8af135c2b0260861c11b0c3d2eeb925c212
NASSABAIN MARCO <marco.nassabain@etu.unistra.fr>
2019-04-02 16:52:37 +0200

    added adding meal to todaysMeals

--- src/views/AddMeal.vue
+++ src/views/AddMeal.vue
@@ -1,9 +1,15 @@
 <template>
     <div class="container">
         <h2>My meals</h2>
-        <div class="meal" v-for="meal in $store.getters.myMeals" :key="meal.id">
+        <div class="meal" v-for="(meal, mealIndex) in $store.getters.myMeals" 
+            :key="meal.id" 
+            @click="togglePortionSize(mealIndex)">
             <h4>{{meal.name}}</h4>
             <span>{{meal.carbs}}g-{{meal.protein}}g-{{meal.fats}}g <strong>{{meal.calories}}kcal</strong></span>
+            <div v-if="clicked == mealIndex" class="portion-size">
+                <input type="number" :value="portionSize" min="0" step="0.5"/>
+                <button id="add-button" @click="addMeal(meal)">Add</button>
+            </div>
         </div>
     </div>
 </template>
@@ -11,16 +17,29 @@
 <script>
 export default {
     name: 'addmeal',
+    components: {
+    },
+    data: function() {
+        return {
+            portionSize: 1,
+            clicked: -1,
+        }
+    },
     methods: {
-        addMeal() {
+        addMeal(meal) {
             this.$store.commit('addMeal', {
-                "name": "Tost",
-                "carbs": 30,
-                "protein": 15,
-                "fats": 10,
-                "calories": 270,
-                "portionSize" : 1,
+                "name": meal.name,
+                "carbs": meal.carbs,
+                "protein": meal.protein,
+                "fats": meal.fats,
+                "calories": meal.calories,
+                "portionSize" : this.portionSize,
             });
+
+            this.$router.push({name: 'home'});
+        },
+        togglePortionSize(index) {
+            this.clicked = index;
         }
     }
 }
@@ -43,5 +62,34 @@ export default {
 {
     margin: 0;
     padding: 0;
+}
+
+.hidden
+{
+    display: none !important;
+}
+
+.portion-size
+{
+    margin-top: 15px;
+    display: flex;
+    justify-content: space-between;
+}
+
+#add-button
+{
+    background-color: #134E5E;
+    border: none;
+    color: white;
+    font-weight: bold;
+    padding: 8px 25px;
+}
+
+.portion-size input[type=number]
+{
+    background-color: white;
+    border: none;
+    padding: 5px 15px;
+    width: 30%;
 }
 </style>
